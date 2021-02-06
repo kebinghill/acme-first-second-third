72f6bc2f1b2ea46c120dd6c5f2b0b4837ad87ae8
Kevin Gil <93kevingil@gmail.com>
2021-02-06 17:16:21 -0500

    completed first weekend project

--- acme.js
+++ acme.js
@@ -1,12 +1,3 @@
-const slots = ['first', 'second', 'third'];
-
-const users = [
-  { id: 1, name: 'moe', slot: 'first' },
-  { id: 2, name: 'larry', slot: 'second' },
-  { id: 3, name: 'curly', slot: 'third' },
-  { id: 4, name: 'lucy', slot: 'third', selected: true },
-];
-
 //List Names
 
 const nameList = document.querySelectorAll('.names');
@@ -17,29 +8,61 @@ const highlightName = [...nameList].forEach((name) => {
   });
 });
 
-//Button Direction Event Listeners
+//Each List
+
+const listOne = document.getElementById('first');
+const listTwo = document.getElementById('second');
+const listThree = document.getElementById('third');
+
+//Button Directions
 
 const buttonL1Forward = document.getElementById('forward-l-1');
 const buttonL2Back = document.getElementById('back-l-2');
 const buttonL2Forward = document.getElementById('forward-l-2');
 const buttonL3Back = document.getElementById('back-l-3');
 
+//Event Listeners
+
 buttonL1Forward.addEventListener('click', function () {
-  console.log('functioning');
+  [...nameList].forEach((name) => {
+    if (
+      name.classList.value === 'names bkg-color' &&
+      name.parentElement === listOne
+    ) {
+      listTwo.appendChild(name);
+    }
+  });
 });
 
 buttonL2Back.addEventListener('click', function () {
-  console.log('Im working');
+  [...nameList].forEach((name) => {
+    if (
+      name.classList.value === 'names bkg-color' &&
+      name.parentElement === listTwo
+    ) {
+      listOne.appendChild(name);
+    }
+  });
 });
 
 buttonL2Forward.addEventListener('click', function () {
-  console.log('Im working');
+  [...nameList].forEach((name) => {
+    if (
+      name.classList.value === 'names bkg-color' &&
+      name.parentElement === listTwo
+    ) {
+      listThree.appendChild(name);
+    }
+  });
 });
 
 buttonL3Back.addEventListener('click', function () {
-  console.log('Im working');
-});
-
-[...document.getElementsByClassName('names')].forEach((name) => {
-  console.log(name.classList.value);
+  [...nameList].forEach((name) => {
+    if (
+      name.classList.value === 'names bkg-color' &&
+      name.parentElement === listThree
+    ) {
+      listTwo.appendChild(name);
+    }
+  });
 });
